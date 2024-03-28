976a8c6440b80a647e7e3b9d910bfcfedbb065f2
Adityachatur <66326241+Adityachatur@users.noreply.github.com>
2024-03-28 12:45:12 +0530

    new chnage

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,19 +1,24 @@
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faBriefcase, faCircleInfo, faHouse, faPhone, faTimes, faWrench } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState, useEffect } from 'react';
 import logo from '../images/Vighnaharta Event  New Logo 1.png';
 import { Link, useLocation } from 'react-router-dom';
+
 const Navbar = () => {
     let location = useLocation();
     useEffect(() => {
     }, [location]);
-
 
     const [showSidebar, setShowSidebar] = useState(false);
 
     const toggleSidebar = () => {
         setShowSidebar(!showSidebar);
     };
+
+    const handleLinkClick = () => {
+        setShowSidebar(false);
+    };
+
 
     return (
         <div>
@@ -40,30 +45,30 @@ const Navbar = () => {
                 {/* Navbar Links */}
                 <div className='hidden list-none cursor-pointer lg:flex space-x-3 items-center'>
                     <ul className='flex space-x-5 text-white text-2xl font-Anek Gurmukhi'>
-                        <li><Link to={'/'} className={`${location.pathname === "/" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Home</Link></li>
-                        <li><Link to={'/about'} className={`${location.pathname === "/about" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>About</Link></li>
-                        <li><Link to={'/service'} className={`${location.pathname === "/service" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Service</Link></li>
-                        <li><Link to={'/projects'} className={`${location.pathname === "/projects" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Projects</Link></li>
-                        <li><Link to={'/contact'} className={`${location.pathname === "/contact" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}> Contact</Link></li>
+                        <li><Link to={'/'} onClick={handleLinkClick} className={`${location.pathname === "/" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Home</Link></li>
+                        <li><Link to={'/about'} onClick={handleLinkClick} className={`${location.pathname === "/about" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>About</Link></li>
+                        <li><Link to={'/service'} onClick={handleLinkClick} className={`${location.pathname === "/service" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Service</Link></li>
+                        <li><Link to={'/projects'} onClick={handleLinkClick} className={`${location.pathname === "/projects" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Projects</Link></li>
+                        <li><Link to={'/contact'} onClick={handleLinkClick} className={`${location.pathname === "/contact" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}> Contact</Link></li>
                     </ul>
                 </div>
             </nav >
 
             {/* Sidebar (Mobile View) */}
             < div className={`lg:hidden fixed inset-0 bg-opacity-50 z-50 transition-transform duration-500 ease-in-out transform ${showSidebar ? 'translate-x-0' : '-translate-x-full'}`}>
-                <div className='fixed inset-y-0 left-0 w-52 bg-white z-50'>
+                <div className='fixed inset-y-0 left-0 w-52 sidebar text-white z-50'>
                     <div className='flex justify-between p-5'>
                         {/* Close Icon */}
                         <button onClick={toggleSidebar} className='flex w-full text-xl justify-end'>
                             <FontAwesomeIcon icon={faTimes} />
                         </button>
                     </div>
-                    <ul className='flex flex-col w-full space-y-2 p-5 text-xl font-Anek Gurmukhi'>
-                        <li><Link to={'/'} className={`${location.pathname === "/" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Home</Link></li>
-                        <li><Link to={'/about'} className={`${location.pathname === "/about" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>About</Link></li>
-                        <li><Link to={'/service'} className={`${location.pathname === "/service" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Service</Link></li>
-                        <li><Link to={'/projects'} className={`${location.pathname === "/projects" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}>Projects</Link></li>
-                        <li><Link to={'/contact'} className={`${location.pathname === "/contact" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}> Contact</Link></li>
+                    <ul className='flex flex-col w-full space-y-2 p-5 text-xl font-Roboto Serif'>
+                        <li><Link to={'/'} onClick={handleLinkClick} className={`${location.pathname === "/" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}> <FontAwesomeIcon icon={faHouse} className='mr-4 items-center' />Home</Link></li>
+                        <li><Link to={'/about'} onClick={handleLinkClick} className={`${location.pathname === "/about" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}><FontAwesomeIcon icon={faCircleInfo} className='mr-4 items-center' />About</Link></li>
+                        <li><Link to={'/service'} onClick={handleLinkClick} className={`${location.pathname === "/service" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}> <FontAwesomeIcon icon={faWrench} className='mr-4 items-center' />Service</Link></li>
+                        <li><Link to={'/projects'} onClick={handleLinkClick} className={`${location.pathname === "/projects" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}><FontAwesomeIcon icon={faBriefcase} className='mr-4 items-center' />Projects</Link></li>
+                        <li><Link to={'/contact'} onClick={handleLinkClick} className={`${location.pathname === "/contact" ? "text-custom-Metallic-gold" : ""} animated-text hover:text-yellow-300`}> <FontAwesomeIcon icon={faPhone} className='mr-4 items-center' />Contact</Link></li>
                     </ul>
                 </div>
             </div >
